bfc1540f9967e7375a54909da63d1dc5e022a558
MSI\my_st <Austin1206@gmail.com>
2023-05-18 17:06:49 +0800

    Work: 1.Login Page(目前能夠將前端資料導入後端，但尚無法導入資料庫內儲存) 2.Reverse Engineer生成PetHeavenDbContext & 各Table的Model

---
+++ FourthTeamProject/Controllers/t_MemberController.cs
@@ -0,0 +1,19 @@
+﻿using FourthTeamProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FourthTeamProject.Controllers
+{
+    public class t_MemberController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(MemberLoginViewModel model)
+        {
+            return Ok(model.t_MemberAccount+model.t_MemberPassword);
+        }
+    }
+}
---
+++ FourthTeamProject/Models/MemberLoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FourthTeamProject.Models
+{
+    public class MemberLoginViewModel
+    {
+        public string t_MemberAccount { get; set; }
+        public string t_MemberPassword { get; set; }
+      
+    }
+}

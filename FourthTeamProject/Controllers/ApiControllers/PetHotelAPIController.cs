3dff9a41452877bd0d6a28b25c5c7ba7a11e3de4
Zing <leafne2315@gmail.com>
2023-05-21 14:41:47 +0800

    add petHotel view and connect db table hotelService

---
+++ FourthTeamProject/Controllers/ApiControllers/PetHotelAPIController.cs
@@ -0,0 +1,41 @@
+﻿using FourthTeamProject.Models;
+using FourthTeamProject.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FourthTeamProject.Controllers.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetHotelAPIController : ControllerBase
+    {
+        private readonly PetHeavenDbContext petHeavenDb;
+
+        public PetHotelAPIController(PetHeavenDbContext petHeavenDb )
+        {
+            this.petHeavenDb = petHeavenDb;
+        }
+
+        [HttpGet]
+        public IActionResult Index() 
+        {
+            var petHotelDB = petHeavenDb.THotelService.ToList();
+            var petHotelList = petHotelDB.Select(data => new PetHotelViewModel()
+            {
+                Service = data.CService,
+                RoomTypeId = data.CRoomTypeId,
+                PetSizeId = data.CPetSizeId,
+                UnitPrice = data.CUnitPrice,
+            });
+         
+
+            return Ok(petHotelList);
+        }
+
+        
+        //public IActionResult getRoomDataById(int roomId)
+        //{
+
+        //}
+    }
+}

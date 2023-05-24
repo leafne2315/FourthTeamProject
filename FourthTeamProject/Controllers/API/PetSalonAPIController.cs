0a448fd7ec07ff09965f4d4e8956aa705f927c94
22_YU <berserker724350@gmail.com>
2023-05-25 00:47:24 +0800

    更新Salon的ViewModel、APIController、View資料索取欄位

--- FourthTeamProject/Controllers/API/PetSalonAPIController.cs
+++ FourthTeamProject/Controllers/API/PetSalonAPIController.cs
@@ -1,5 +1,6 @@
 ﻿using FourthTeamProject.Models;
 using FourthTeamProject.Models.ViewModel;
+using FourthTeamProject.PetHeavenModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,26 +11,35 @@ namespace FourthTeamProject.Controllers.API
     [ApiController]
     public class PetSalonAPIController : ControllerBase
     {
-        //private readonly PetHeavenDbContext _context;
-        //public PetSalonAPIController(PetHeavenDbContext context)
-        //{
-        //    _context = context;
-        //}
-        //public async Task<IEnumerable<SalonViewModel>> GetPetSalon()
-        //{
-        //    var temp =  _context.TSalonType.Select(option => new SalonViewModel
-        //    {
-        //        CSalonId = option.CSalonId,
-        //        CSalonName = option.CSalonName,
-        //        CSalonContet = option.CSalonContet
-        //    });
-        //    return temp;
-        //}
+        private readonly PetHeavenDbContext _context;
+        public PetSalonAPIController(PetHeavenDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<SalonViewModel>> GetPetSalon()
+        {
+            var temp = _context.Salon
+                .Join(_context.SalonSolution,Salon => Salon.SalonId
+                , SalonSolution=>SalonSolution.SalonSolutionId
+                ,(Salon,SalonSolution) => new {Salon=Salon,SalonSolution=SalonSolution})
+                .Select(joinresult => new SalonViewModel
+            {
+                SalonCatagoryName=joinresult.Salon.SalonCatagory.SalonCatagoryName,
+                SalonSolutionName=joinresult.SalonSolution.SalonSolutionName,
+                SalonSolutionDiscription=joinresult.SalonSolution.SalonSolutionDiscription,
+                SalonSolutionPrice=joinresult.SalonSolution.SalonSolutionPrice,
+                SalonName=joinresult.Salon.SalonName,
+                SalonImagePath=joinresult.Salon.SalonImagePath,
+
+
+            });
+            return temp;
+        }
 
         //[HttpPut]
         //public async Task<string> PutPetSalon(int id, [FromBody] SalonViewModel SalonDTO)
         //{
         //}
-            
+
     }
 }

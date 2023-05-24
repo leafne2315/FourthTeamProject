c051176955ac7ab3ba2ea2366927be8a5017cfbb
Mike <mike86831@gmail.com>
2023-05-24 11:48:12 +0800

    新增ProductAPIController&ViewModel 並將ProductOrder Model加上。

---
+++ FourthTeamProject/Controllers/API/PetProdcutAPIController.cs
@@ -0,0 +1,50 @@
+﻿using FourthTeamProject.Models;
+using FourthTeamProject.Models.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FourthTeamProject.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PetProdcutAPIController : ControllerBase
+    {
+
+        readonly PetHeavenDbContext _db;
+
+
+        public PetProdcutAPIController(PetHeavenDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index(ProductViewModel product)
+        {
+            var result = _db.Product.Select(p => new ProductViewModel
+            {
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                ProductContent = p.ProductContent,
+                ProductImage = p.ProductImage,
+                ProductCatagory = p.ProductCatagory,
+            });
+
+            return Ok(result);
+
+
+
+        }
+        
+
+        
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ FourthTeamProject/Models/ViewModel/ProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace FourthTeamProject.Models.ViewModel
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+        public int ProductTypeId { get; set; }
+        public string ProductName { get; set; }   
+        public string ProductContent { get; set; }
+        public int UnitPrice { get; set; }
+        public int ProductCatagoryId { get; set; }
+
+        public virtual ProductCatagory ProductCatagory { get; set; }
+        public virtual ProductType ProductType { get; set; }
+        public virtual ICollection<ProductImage> ProductImage { get; set; }
+        
+    }
+}

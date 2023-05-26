8d4d72a0417f711a1bbba84732c25b4f9e1e013c
MSI\my_st <Austin1206@gmail.com>
2023-05-26 14:55:31 +0800

    更新vs版本

---
+++ FourthTeamProject/Controllers/ProductsController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FourthTeamProject.Models;
+using FourthTeamProject.PetHeavenModels;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+
+namespace FourthTeamProject.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly PetHeavenDbContext _context;
+
+        public ProductsController(PetHeavenDbContext context)
+        {
+			_context = context;
+        }
+
+		// GET: Products
+	
+		public async Task<IActionResult> Index()
+        {
+            var petHeavenDbContext = _context.Product.Include(p => p.ProductCatagory).Include(p => p.ProductType);
+            return View(await petHeavenDbContext.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .Include(p => p.ProductCatagory)
+                .Include(p => p.ProductType)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductCatagoryId"] = new SelectList(_context.ProductCatagory, "ProductCatagoryId", "ProductCatagoryName");
+            ViewData["ProductTypeId"] = new SelectList(_context.ProductType, "ProductTypeId", "ProductTypeName");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,ProductTypeId,ProductName,ProductSpecification,ProductContent,UnitPrice,Stock,ProductStatus,ProductCatagoryId")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductCatagoryId"] = new SelectList(_context.ProductCatagory, "ProductCatagoryId", "ProductCatagoryName", product.ProductCatagoryId);
+            ViewData["ProductTypeId"] = new SelectList(_context.ProductType, "ProductTypeId", "ProductTypeName", product.ProductTypeId);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductCatagoryId"] = new SelectList(_context.ProductCatagory, "ProductCatagoryId", "ProductCatagoryName", product.ProductCatagoryId);
+            ViewData["ProductTypeId"] = new SelectList(_context.ProductType, "ProductTypeId", "ProductTypeName", product.ProductTypeId);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductTypeId,ProductName,ProductSpecification,ProductContent,UnitPrice,Stock,ProductStatus,ProductCatagoryId")] Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductCatagoryId"] = new SelectList(_context.ProductCatagory, "ProductCatagoryId", "ProductCatagoryName", product.ProductCatagoryId);
+            ViewData["ProductTypeId"] = new SelectList(_context.ProductType, "ProductTypeId", "ProductTypeName", product.ProductTypeId);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .Include(p => p.ProductCatagory)
+                .Include(p => p.ProductType)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Product == null)
+            {
+                return Problem("Entity set 'PetHeavenDbContext.Product'  is null.");
+            }
+            var product = await _context.Product.FindAsync(id);
+            if (product != null)
+            {
+                _context.Product.Remove(product);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+          return (_context.Product?.Any(e => e.ProductId == id)).GetValueOrDefault();
+        }
+    }
+}

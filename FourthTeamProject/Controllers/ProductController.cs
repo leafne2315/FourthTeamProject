b27d3bde8c4aa685de507b38e2949df4b337252f
Mike <mike86831@gmail.com>
2023-06-03 16:33:45 +0800

    1.新增商品結帳頁2.結帳生成訂單3.結帳清空購物車4.產品詳細頁面

--- FourthTeamProject/Controllers/ProductController.cs
+++ FourthTeamProject/Controllers/ProductController.cs
@@ -31,31 +31,17 @@ namespace FourthTeamProject.Controllers
 
 		}
 
-		//[HttpPost]
-		//public IActionResult BatchDelete(int[] selectedIds)
-		//{
-		//    if (selectedIds == null || selectedIds.Length == 0)
-		//    {
-		//        // 如果没有选择任何商品，则返回错误消息
-		//        return Json(new { success = false, message = "请选择至少一个商品来删除。" });
-		//    }
+        public IActionResult OrderDone()
+        {
+            return View();
+        }
 
-		//    try
-		//    {
-		//        // 根据选中的商品Id数组进行批量删除
-		//        var productsToDelete = _db.Product.Where(p => selectedIds.Contains(p.ProductId));
-		//        _db.Product.RemoveRange(productsToDelete);
-		//        _db.SaveChanges();
+        public IActionResult ProductDetail()
+        {
+            return View();
+        }
 
-		//        // 返回成功消息
-		//        return Json(new { success = true, message = "批量删除成功。" });
-		//    }
-		//    catch (Exception ex)
-		//    {
-		//        // 返回错误消息
-		//        return Json(new { success = false, message = "删除商品时出现错误：" + ex.Message });
-		//    }
-		//}
 
-	}
+
+    }
 }

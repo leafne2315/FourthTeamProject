73c410304d67bdee4150210eda58707338311b82
Zing <leafne2315@gmail.com>
2023-06-07 19:02:41 +0800

    旅店細節頁初步完成
    1.相片未完成
    2.串接資料到訂購頁

---
+++ FourthTeamProject/Models/ViewModel/PetHotelServiceViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FourthTeamProject.PetHeavenModels;
+
+namespace FourthTeamProject.Models.ViewModel
+{
+    public class PetHotelServiceViewModel
+    {
+        public int HotelId { get; set; }
+        public int HotelServiceId { get; set; }
+        public HotelService HotelService { get; set; }
+    }
+}

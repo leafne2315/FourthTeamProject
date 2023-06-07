0f9af5e3d4bf9624bbdec1fd0448b1b6d612ff56
Zing <leafne2315@gmail.com>
2023-06-07 21:59:33 +0800

    待修正 controller function

--- FourthTeamProject/Controllers/PetHotelController.cs
+++ FourthTeamProject/Controllers/PetHotelController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FourthTeamProject.Models.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
 
 namespace FourthTeamProject.Controllers
 {
@@ -12,13 +15,20 @@ namespace FourthTeamProject.Controllers
         }
         public IActionResult HotelDetail(int hotelId)
         {
-
             return View(hotelId);
         }
         
         public IActionResult HotelOrder(int hotelId) 
         { 
             return View(); 
+        }
+
+        public IActionResult SendDataToOrder(HotelOrderDetailViewModel orderDetailData)
+        {
+            string serializedData = JsonConvert.SerializeObject(orderDetailData);
+            HttpContext.Session.SetString("OrderData", serializedData);
+
+            return Ok(serializedData);
         }
     }
 }

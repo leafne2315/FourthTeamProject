b388ce464adac744e97b07d63d3271a3d5428e61
MSI\my_st <Austin1206@gmail.com>
2023-06-09 03:21:47 +0800

    1.後臺訂單管理及明細功能修復 2.商品CRUD版面調適完成

---
+++ FourthTeamProject/Areas/Admin/ViewModels/ProductOrderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FourthTeamProject.PetHeavenModels;
+
+
+namespace FourthTeamProject.Areas.Admin.ViewModels
+{
+    public class ProductOrderViewModel
+    {
+        public IEnumerable<ProductOrder> Orders { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+
+}
+
---
+++ FourthTeamProject/Models/ViewModel/ProductOrderDetailViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FourthTeamProject.PetHeavenModels;
+
+namespace FourthTeamProject.Models.ViewModel
+{
+    public class ProductOrderDetailViewModel
+    {
+        public IEnumerable<ProductOrderDetail> Orders { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+}

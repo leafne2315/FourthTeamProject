28537afd5b5ae7a6876e7832cc9cca39060e225c
Zing <leafne2315@gmail.com>
2023-06-11 14:20:03 +0800

    最後版型

--- FourthTeamProject/Controllers/PetHotelController.cs
+++ FourthTeamProject/Controllers/PetHotelController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Newtonsoft.Json;
+using System.Security.Claims;
 
 namespace FourthTeamProject.Controllers
 {
@@ -19,8 +20,24 @@ namespace FourthTeamProject.Controllers
         }
         
         public IActionResult HotelOrder(int hotelId) 
-        { 
-            return View(); 
+        {
+            IConfiguration Config = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
+            // 產生測試資訊
+            ViewData["MerchantID"] = Config.GetSection("MerchantID").Value;//商店代號
+            ViewData["MerchantOrderNo"] = DateTime.Now.ToString("yyyyMMddHHmmss");  //訂單編號
+            ViewData["ExpireDate"] = DateTime.Now.AddDays(3).ToString("yyyyMMdd"); //繳費有效期限
+            ViewData["ReturnURL"] = $"{Request.Scheme}://{Request.Host}/Product/OrderDone"; //支付完成返回商店網址
+                                                                                            //ViewData["CustomerURL"] = $"{Request.Scheme}://{Request.Host}{Request.Path}Home/CallbackCustomer"; //商店取號網址
+                                                                                            //ViewData["NotifyURL"] = $"{Request.Scheme}://{Request.Host}{Request.Path}Home/CallbackNotify"; //支付通知網址
+                                                                                            //ViewData["ClientBackURL"] = $"{Request.Scheme}://{Request.Host}{Request.Path}"; //返回商店網址
+
+            var email = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+            if (email != null)
+            {
+                ViewBag.Email = email;
+                return View();
+            }
+            return Redirect("/Member/Login");
         }
 
         
